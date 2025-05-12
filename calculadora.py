2e2906cfc89bb20ad5035135a51d3a9087382723
Alan Kristofer <152701080+alankristofer@users.noreply.github.com>
2025-05-11 21:17:38 -0300

    colocando as bibliotecas, cores e janelas
    foi adicionado no codigo, as bibliotecas que irao ser utilizadas, cores, e a definiçao da janela da aplicaçao.

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
+#cores
+cor1 = "#3b3b3b" # black/ preto
+cor2 = "#feffff" # white/ branco
+cor3 = "#38576b" # azul forte
+cor4 = "#ECEFF1" # cinzento
+cor5 = "#FFAB40" # Laranja/ orange
+cor6 = "#0abef5" # azul claro
+
+#janela da aplicaçao
+janela = Tk()
+janela.title("Calculadora")
+janela.geometry("235x301")
+janela.config(background=cor1) #fundo do frame
