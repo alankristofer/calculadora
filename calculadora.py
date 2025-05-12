ace84bd9c2ebd5588816f058d778ff861de880a2
Alan Kristofer <152701080+alankristofer@users.noreply.github.com>
2025-05-12 01:11:16 +0000

    adicionando as funçoes dos numeros, e separando a tela em frames.

--- calculadora.py
+++ calculadora.py
@@ -15,3 +15,51 @@ janela = Tk()
 janela.title("Calculadora")
 janela.geometry("235x301")
 janela.config(background=cor1) #fundo do frame
+
+#separando a tela em frames.
+frame_tela = Frame(janela, width=235, height=50, bg=cor3)
+frame_tela.grid(row=0, column=0)
+
+frame_corpo = Frame(janela, width=235, height= 268)
+frame_corpo.grid(row=1, column=0)
+
+
+# variavel todos os valores
+
+todos_valores = ''
+def inserir_valor_texto(texto):
+    global todos_valores
+    todos_valores += texto
+    valor_texto.set(todos_valores)
+
+
+def entrar_numero0():
+    inserir_valor_texto('0') 
+
+def entrar_numero1():
+    inserir_valor_texto('1')
+
+def entrar_numero2():
+    inserir_valor_texto('2')
+
+def entrar_numero3():
+    inserir_valor_texto('3')
+
+def entrar_numero4():
+    inserir_valor_texto('4')    
+
+def entrar_numero5():
+    inserir_valor_texto('5')    
+
+def entrar_numero6():
+    inserir_valor_texto('6')    
+
+def entrar_numero7():
+    inserir_valor_texto('7')
+
+def entrar_numero8():
+    inserir_valor_texto('8')    
+
+def entrar_numero9():
+    inserir_valor_texto('9')
+
